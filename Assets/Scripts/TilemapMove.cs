ca99833f4cb83bed12182fc356a9d8d67e09583f
黄子安 <9764817+aaaz718@user.noreply.gitee.com>
2023-06-08 22:19:38 +0800

    添加计时功能，同时增加关卡障碍物设计框架，修改部分脚本的逻辑

--- Assets/Scripts/TilemapMove.cs
+++ Assets/Scripts/TilemapMove.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class TilemapMove : MonoBehaviour
 {
     private GameObject[] tilemap=new GameObject[32];
+    public GameObject mushRoomPreferb;
+    private GameObject mr;
+    private float timer = 0f;
     public float Speed=0.05f;
     private float ystart,zstart;
     float lasttime;
     void Start()
     {
+        StartCoroutine(StartTimer());
         lasttime = Time.time;
         for (int i=0;i<2;++i)
         {
@@ -29,12 +33,44 @@ public class TilemapMove : MonoBehaviour
                 if (tilemap[i].transform.position.x < -27.7f)
                 {
                    tilemap[i].transform.position = new Vector3(26.2f, ystart, zstart);
+                   if(mr==null) mr = Instantiate(mushRoomPreferb, transform.position + new Vector3(20.0f, -0.5f, 0), Quaternion.identity);
                 }
                 else
                 {
-                    tilemap[i].transform.position -= new Vector3(Speed, 0, 0);
+                    tilemap[i].transform.position -= new Vector3(Time.deltaTime*Speed, 0, 0);
                 }
             }
         }
+        BarrierController((int)timer);
+    }
+    void BarrierController(int t)
+    {
+        Debug.Log(t);
+        if(t>=60)//finished
+        {
+
+        }
+        else
+        {
+
+        }
+
+    }
+    void OnGUI() // 在屏幕上显示计时器的数值
+    {
+        int t;
+        if(timer<=3) t=0;
+        else t=(int)timer-3;
+        GUI.Label(new Rect(10, 10, 100, 20), "Timer: " + t.ToString("F2"));
+    }
+    IEnumerator StartTimer()
+    {
+        while (true)
+        {
+            timer = Time.time;
+            yield return null;
+        }
     }
 }
+
+
